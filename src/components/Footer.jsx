ea7c72c7742584329746c0534a124aeaaff8e5f8
Suraj Gopal <surajcancode@gmail.com>
2023-03-01 11:44:47 -0500

    react new

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+import '../styles/Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="socialMedia">
+        <LinkedInIcon />
+        <GitHubIcon />
+      </div>
+      <p> &copy; 2023 surajgopal.tech </p>
+    </div>
+  )
+}
+
+export default Footer;
