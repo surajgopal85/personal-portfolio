6645464ca819143ebb0cce9ddf9e76283cf4b383
Suraj Gopal <surajcancode@gmail.com>
2023-03-01 15:07:42 -0500

    added schooling to exp page - timeline component, styling of exp page

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+
+import SchoolIcon from '@material-ui/icons/School';
 
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
-}
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+        className="vertical-timeline-element--education"
+        date='2003-2007'
+        iconStyle={ {background: "#3e497a", color: "#fff"} }
+        icon={<SchoolIcon />}>
+          <h3>Amherst College (Amherst, MA)</h3>
+          <p>B.A. Music (Composition)</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+        className="vertical-timeline-element--education"
+        date='2011-2013'
+        iconStyle={ {background: "#3e497a", color: "#fff"} }
+        icon={<SchoolIcon />}>
+          <h3>LIU-Brooklyn (Brooklyn, NY)</h3>
+          <p>M.S., Special Education</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+        className="vertical-timeline-element--education"
+        date='Oct 2022 - Dec 2022'
+        iconStyle={ {background: "#3e497a", color: "#fff"} }
+        icon={<SchoolIcon />}>
+          <h3>Le Wagon (Remote)</h3>
+          <p>Certificate of Completion, Web Development</p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
+};
 
 export default Experience;
